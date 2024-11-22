929ba4f8c98a32c98222e1b0981c18db66be5a0a
devadg <opponamer@gmail.com>
2024-11-22 03:45:15 +0100

    shipped new func

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require("cors");
+const sendEmail = require("./utils/mail");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
